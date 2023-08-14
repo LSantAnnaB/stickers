baa759df6873d6ec2bc828f70273e4f2abbcba7d
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-08-14 15:57:11 -0300

    ApiRequest

--- src/App.java
+++ src/App.java
@@ -3,16 +3,26 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
-        URI endereco = URI.create(url);
+        URI adress = URI.create(url);
         var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
+        var request = HttpRequest.newBuilder(adress).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
+        var parse = new JsonParser();
+        List<Map<String, String>> movieList = parse.parse(body);
+
+        for (Map<String, String> movie : movieList) {
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+
+        }
     }
 }
